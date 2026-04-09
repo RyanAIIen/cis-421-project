00695e25a84f609a8e7b6defacd7f76ff708fb73
Ryan Allen <allenrya@umich.edu>
2026-04-09 19:35:50 -0400

    add theming

--- web/pharmacydb/settings.py
+++ web/pharmacydb/settings.py
@@ -76,10 +76,11 @@ ROOT_URLCONF = "pharmacydb.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "pharmacydb" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -138,4 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'pharmacydb/static/'),)
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / '.static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / '.media'
